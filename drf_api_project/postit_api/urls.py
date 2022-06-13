9b09d1d5189a12cdf3fb83328627c50a973a5647
infohata <infohata@gmail.com>
2022-06-13 18:46:36 +0300

    post list api

--- drf_api_project/postit_api/urls.py
+++ drf_api_project/postit_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    
+    path('api/posts/', views.PostListAPI.as_view()),
 ]
--- drf_api_project/postit_api/views.py
+++ drf_api_project/postit_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models
+from . import serializers
 
-# Create your views here.
+
+class PostListAPI(generics.ListAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
