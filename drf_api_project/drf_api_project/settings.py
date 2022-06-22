467f3347a82a4f71c15e04457b18dadb6b4f1530
infohata <infohata@gmail.com>
2022-06-22 19:53:51 +0300

    postgres veikia

--- drf_api_project/drf_api_project/settings.py
+++ drf_api_project/drf_api_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*4$qn12gl4li$gl!56u6xqb29!ids()k$b29*ngc4bq_j(@zyy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "postit.local"]
 
 
 # Application definition
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'drf_api_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "postgres",
+        "NAME": "postit",
+        "USER": "postit",
+        "PASSWORD": "nesakysiu",
+        "PORT": 5432
     }
 }
 
